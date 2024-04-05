a451cae825f1bf53604361b4e265b5e0327b9309
Shpetim <kadrijashpetim@gmail.com>
2024-04-05 14:55:25 +0200

    include creds & mode cors

--- src/resources/pathfinder.ts
+++ src/resources/pathfinder.ts
@@ -10,6 +10,11 @@ export class PathFinder extends Base {
     return fetch('http://localhost:4000/status', {
       credentials: 'include',
       mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization:
+          'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPbmxpbmUgSldUIEJ1aWxkZXIiLCJpYXQiOjE3MTIyMzY2MDMsImV4cCI6MTc0Mzc3MjYwMywiYXVkIjoid3d3LmV4YW1wbGUuY29tIiwic3ViIjoianJvY2tldEBleGFtcGxlLmNvbSIsIkdpdmVuTmFtZSI6IkpvaG5ueSIsIlN1cm5hbWUiOiJSb2NrZXQiLCJFbWFpbCI6Impyb2NrZXRAZXhhbXBsZS5jb20iLCJSb2xlIjpbIk1hbmFnZXIiLCJQcm9qZWN0IEFkbWluaXN0cmF0b3IiXX0.ZeFIty-RmaR5jyo5zit8EJMaQ_8xevhr1w9tV5osb9M'
+      }
     }).then(response => response.json());
   }
 }
