8a50c9b79c952c52467569f32e741bffa53a5529
Shpetim <kadrijashpetim@gmail.com>
2024-04-15 15:27:48 +0200

    return constructed payload

--- src/resources/pathfinder.ts
+++ src/resources/pathfinder.ts
@@ -4,6 +4,41 @@ import { Base } from './base';
 export class PathFinder extends Base {
   async getNext(config: IGetNext): Promise<any> {
     const { path, payload, token } = config || {};
-    return await this.post(path, payload, token);
+    const nextPath: any = await this.post(path, payload, token);
+
+    if (!nextPath.path.length) {
+      return {
+        nextActionType: 'error',
+      };
+    }
+
+    const detailsNextPath = nextPath.path.find((p) => p.id === nextPath.next);
+
+    if (!detailsNextPath) {
+      return {
+        nextActionType: 'error',
+      };
+    }
+
+    return {
+      nextActionType: 'continue',
+      url: getUrl(detailsNextPath.name, detailsNextPath.next),
+      activityTemplateSlug: detailsNextPath.name,
+      activityTemplateGameName: detailsNextPath.name,
+      lessonDisplayName: detailsNextPath.name,
+    };
   }
 }
+
+const getUrl = (gameName: string, gameId?: string) => {
+  switch (gameName) {
+    case 'Swap-Hop':
+      return `https://https://platform-game-viewer.dev.magpie.org/${gameId}`;
+    case 'SWR':
+      return 'https://assessments.dev.magpie.org/swr/tutorial';
+    case 'Letter-Sounds':
+      return 'https://assessments.dev.magpie.org/letter-sounds/tutorial';
+    default:
+      return '';
+  }
+};
