2231fa2d976d00e3de65e345fc5715e3ade9702f
Shpetim <kadrijashpetim@gmail.com>
2024-04-15 18:09:15 +0200

    pathfinder return to hte old form

--- src/resources/pathfinder.ts
+++ src/resources/pathfinder.ts
@@ -1,44 +1,22 @@
-import { IGetNext, NextActionType, NextPathResponse } from '../types';
+import { IGetNext } from '../types';
 import { Base } from './base';
 
 export class PathFinder extends Base {
   async getNext(config: IGetNext): Promise<any> {
     const { path, payload, token } = config || {};
-    const nextPath: NextPathResponse = await this.post(path, payload, token);
-
-    if (!nextPath.path.length) {
-      return {
-        nextActionType: NextActionType.ERROR,
-      };
-    }
-
-    const detailsNextPath = nextPath.path.find((p) => p.id === nextPath.next);
-
-    if (!detailsNextPath) {
-      return {
-        nextActionType: NextActionType.ERROR,
-      };
-    }
-
-    return {
-      nextActionType: NextActionType.CONTINUE,
-      url: getUrl(detailsNextPath.name, detailsNextPath.id),
-      activityTemplateSlug: detailsNextPath.name,
-      activityTemplateGameName: detailsNextPath.name,
-      lessonDisplayName: detailsNextPath.name,
-    };
+    return await this.post(path, payload, token);
   }
 }
 
-const getUrl = (gameName: string, gameId?: string) => {
-  switch (gameName) {
-    case 'Swap-Hop':
-      return `https://https://platform-game-viewer.dev.magpie.org/${gameId}`;
-    case 'SWR':
-      return 'https://assessments.dev.magpie.org/swr/tutorial';
-    case 'Letter-Sounds':
-      return 'https://assessments.dev.magpie.org/letter-sounds/tutorial';
-    default:
-      return '';
-  }
-};
+// const getUrl = (gameName: string, gameId?: string) => {
+//   switch (gameName) {
+//     case 'Swap-Hop':
+//       return `https://https://platform-game-viewer.dev.magpie.org/${gameId}`;
+//     case 'SWR':
+//       return 'https://assessments.dev.magpie.org/swr/tutorial';
+//     case 'Letter-Sounds':
+//       return 'https://assessments.dev.magpie.org/letter-sounds/tutorial';
+//     default:
+//       return '';
+//   }
+// };
